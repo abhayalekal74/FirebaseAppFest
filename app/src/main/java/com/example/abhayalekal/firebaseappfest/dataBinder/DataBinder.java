5133396e1b178d39d2d1006078b65a8d220b4dad
gunjit <dgunjit14@gmail.com>
2017-06-21 11:05:17 +0530

    dadded dependencies

---
+++ app/src/main/java/com/example/abhayalekal/firebaseappfest/dataBinder/DataBinder.java
@@ -0,0 +1,30 @@
+package com.example.abhayalekal.firebaseappfest.dataBinder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Class for binding view and data
+ * <p/>
+ * Created by yqritc on 2015/03/01.
+ */
+abstract public class DataBinder<T extends RecyclerView.ViewHolder> {
+
+    private DataBindAdapter mDataBindAdapter;
+
+    public DataBinder(DataBindAdapter dataBindAdapter) {
+        mDataBindAdapter = dataBindAdapter;
+    }
+
+
+
+    abstract public T newViewHolder(ViewGroup parent);
+
+    abstract public void bindViewHolder(T holder, int position);
+
+
+    public final void notifyDataSetChanged() {
+        mDataBindAdapter.notifyDataSetChanged();
+    }
+    
+}
